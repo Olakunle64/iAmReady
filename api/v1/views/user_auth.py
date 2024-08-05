7d9a454e3072dd2e4b02d93167d45ef75184e77a
Olakunle64 <133286231+Olakunle64@users.noreply.github.com>
2024-08-05 11:40:55 +0100

    Update user_auth.py

--- api/v1/views/user_auth.py
+++ api/v1/views/user_auth.py
@@ -38,8 +38,8 @@ def login():
     
     # add the session_id to cookie
     response = make_response(jsonify({"message": "Logged in"}))
-    response.set_cookie('session_id', session_id, httponly=True, secure=True, samesite=None)
-    response.set_cookie('user_type', user_type, httponly=True, secure=True, samesite=None)
+    response.set_cookie('session_id', session_id)
+    response.set_cookie('user_type', user_type)
     # response.headers.add('Set-Cookie','cross-site-cookie=bar; SameSite=None; Secure')
     return response
 
